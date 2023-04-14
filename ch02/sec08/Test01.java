f41c70eb0b55f0e2470024791c6b12d69c86dabb
rkepdls <rkepdls@naver.com>
2023-04-14 17:32:33 +0900

    연산과 변환

---
+++ ch02/sec08/Test01.java
@@ -0,0 +1,16 @@
+package ch02.sec08;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 3;
+		
+		double c = 0;
+		
+		c= a/(float)b;
+		
+		System.out.printf("%4.4f",c);
+	}
+
+}
