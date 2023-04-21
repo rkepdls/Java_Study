73d8f671f88cc74ad65b4fcd910287fd4acd921e
rkepdls <rkepdls@naver.com>
2023-04-21 16:18:05 +0900

    폴더 새로지정 테스트

---
+++ ch06/sec06/Car.java
@@ -0,0 +1,11 @@
+package ch06.sec06;
+
+public class Car {
+	String company = "현대자동차";
+	String model = "그랜져";
+	int speed;
+	boolean start;
+	
+	
+
+}
---
+++ ch06/sec07/CarExample.java
@@ -0,0 +1,14 @@
+package ch06.sec07;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		//Car객체 생성
+		//Car myCar = new Car();
+		Car myCar = new Car("그랜져", "검정", 250);
+		
+		//
+		
+	}
+
+}
